659981b5434c46d6274c70f5354958b77e0b8380
oscaregr <bavuey@hotmail.com>
2019-10-18 16:37:49 -0500

    suvido

--- 2p/calendar.js
+++ 2p/calendar.js
@@ -47,15 +47,3 @@ class Calendar{
     console.log("el dia, mas el plus")
     console.log(c.todayPlus(3,10))
 })()
-    /*
-    friday13(year){
-        let count = 0;
-    for (let month=0; month<12; month++) {
-        let d = new Date(year,month,13);
-        if(d.getDay() == 5){
-          count++;
-       }
-    }
-    return count;   
-    }
-    */
--- 2p/progr_parcial.js
+++ 2p/progr_parcial.js
@@ -112,6 +112,18 @@ bool isLeapYear(y)
 int todayPlus(d,n) /// imprime el dia incremenardo asignado del dia correspondiente
 int firstDayYear(y)
 //day(d,m,y)
+ 
+    friday13(year){
+        let count = 0;
+    for (let month=0; month<12; month++) {
+        let d = new Date(year,month,13);
+        if(d.getDay() == 5){
+          count++;
+       }
+    }
+    return count;   
+    }
+    
 
 3. 
 Card
