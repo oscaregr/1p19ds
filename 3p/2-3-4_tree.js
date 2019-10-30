00f6bc0c613e86a8487f30e5e003af5ab33edcb7
oscaregr <bavuey@hotmail.com>
2019-10-29 23:24:56 -0600

    casi sale

--- 3p/2-3-4_tree.js
+++ 3p/2-3-4_tree.js
@@ -1,28 +1,35 @@
 class two_three_four{
     constructor(){
         this.tree = [[40,null,null,"|"],
-        [20,null,null,"|"],
-        [80,null,null,"|"],
+        [null,null,null,"|"],
+        [null,null,null,"|"],
         [null,null,null,"|"],
         [null,null,null,"|"]]
-        this.p =0
+        this.p = 1
         this.valores = [0,0,0]
         this.re = [null]
     }
     add(x){
         let v = this.valores
+        //let po = 0
         for(let i = 0 ; i< this.tree.length; i++){
             if(rep(x)){
                 if(this.tree[0][i] != null ){ //&& i <2
                 
-                    if (x < this.tree[0][i]){
-                        this.p = i+1
-                        this.acomo(x)
-                        
+                    if (x > this.tree[0][i]){
+                        this.p+=1
+                        if(this.p === 4){
+                            
+                            this.acomo(x)
+                        }
                     }
-                    else if ( x > this.tree[0][i]){
-                        this.p = i + 2
+                    else {
+                       // this.p = i + 2
+                        //this.acomo(x)
+                       
+                        
                         this.acomo(x)
+                        break
                         
                     } 
                 }
@@ -37,25 +44,25 @@ class two_three_four{
             }
         }
         function rep (r){
-            
             let t = null
-            for(let i = 0; i<=v.length-1; i++){
+            for(let i = 0; i<v.length; i++){
                 if(r != v[i]){
                     t = true
                 }else{
                     t = false
+                    break
                 }
             }
             if(t){
-                v[v.length] = r
                 return true
             }else {return false}
         }
-        this.valores = v
+        
     }
     orde(){
         let m = null
-        if(this.tree[this.p][1] != null || this.tree[this.p][2] != null){
+        let pi = this.p
+        if(this.tree[this.p][1] != null || this.tree[this.p][2] != null || this.tree[this.p][0] != null){
             for(let i = 0; i<2;i++){
                 if(this.tree[this.p][i] > this.tree[this.p][i+1] ){
                 m = this.tree[this.p][i]
@@ -64,18 +71,23 @@ class two_three_four{
                 }
 
             }
+            this.p = 1
+            //return
         }
-        if(this.tree[this.p][0] != null &&this.tree[this.p][1] != null &&this.tree[this.p][2] != null){
-            m = this.tree[this.p][1]
-            this.tree[this.p][1] = null
-            this.p = 0
-            this.acomo(m)
+        if(this.tree[pi][0] != null && this.tree[pi][1] != null && this.tree[pi][2] != null){
+            if(pi != 0){
+                m = this.tree[pi][1]
+                this.tree[pi][1] = null
+                this.p = 0
+                this.acomo(m)
+                this.reorden(m)
+            }
         }
 
     }
     acomo(x){
         let t = null
-        for(let i =0; i<2;i++){
+        for(let i =0; i<=2;i++){
              if(this.tree[this.p][i] === x){
                 console.log("ese valor ya esta en el arbol")
                 t = false
@@ -83,23 +95,45 @@ class two_three_four{
             }else{t = true}
         }
         if(t){
-            for(let i = 0; i<2;i++){
+            for(let i = 0; i<=2;i++){
                 if(this.tree[this.p][i] === null){
                     this.tree[this.p][i] = x 
+                    this.valores[this.valores.length] = x
                     this.orde()
+                    break
                 }
             }
         }
         
     }
+    reorden(x){
+        let li = this.valores
+        li.splice(li.indexOf(x),1)
+        li.splice(li.indexOf(x),1)
+        this.re = li
+        this.valores = [0]
+        this.tree[1] = [null,null,null,"|"]
+        this.tree[2] = [null,null,null,"|"]
+        this.tree[3] = [null,null,null,"|"]
+        this.tree[4] = [null,null,null,"|"]
+        for(let i = 0; i< this.re.length; i++){
+            this.add(this.re[i])
+        }
+        this.re = [0]
+    }
 }
 
-(function(){
+
     let a = new two_three_four()
     a.add(20)
     a.add(80)
     a.add(65)
     a.add(90)
-    a.add(10)
+    a.add(50)
+    a.add(30)
+    a.add(35)
+    a.add(32)
+    
+    console.log(a.valores)
+   // a.reorden()
     console.log(a.tree)
-})()
